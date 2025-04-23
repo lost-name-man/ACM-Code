9e1918dc51dd7990331a543d47ebf6d3a6994d1e
lost-name-man <3014326408@qq.com>
2025-04-23 19:41:00 +0800

    8 init

---
+++ DingPa/6/8.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <iomanip>
+
+#define int long long
+#define INF 1e9
+#define endl '\n'
+#define MOD (int)(1000000007)
+
+const int N = 1000005;
+const int M = N * 2;
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int num;
+    cin >> num;
+    int ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int nexn;
+        cin >> nexn;
+        ans += num * nexn;
+        ans %= MOD;
+        num += nexn;
+        num %= MOD;
+    }
+    int cnt = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        cnt *= i;
+        cnt %= MOD;
+    }
+    cnt /= 2;
+
+    cout << ans << ' ' << cnt << endl;
+}
+
+signed main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    int T = 1;
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+    return 0;
+}
