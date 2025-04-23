97c3669f3dee6149b211cdba614d3cf1eb497d3b
lost-name-man <3014326408@qq.com>
2025-04-23 19:20:26 +0800

    3 init

--- DingPa/6/3.cpp
+++ DingPa/6/3.cpp
@@ -28,6 +28,37 @@ using namespace std;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> arr(n + 5);
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        arr[i].first = a;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        arr[i].second = a;
+    }
+    int r = 1;
+    int sum = 0;
+    int ans = 0;
+    for (int l = 1; l <= n; l++)
+    {
+        while (arr[r].first + arr[r].second == arr[l].first + arr[l].second)
+        {
+
+            sum += arr[r].first;
+
+            r++;
+        }
+        ans = max(ans, sum * (r - l));
+        sum -= arr[l].first;
+    }
+    cout << ans << endl;
 }
 
 signed main()
