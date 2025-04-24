ef6bfab930759348b7bcd94238a71ca2b33bf752
BorderSu <1085626434@qq.com>
2025-04-24 19:42:29 +0800

    8-init

---
+++ DingPa/7/8.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <iomanip>
+
+#define int long long
+#define INF 1e18
+#define endl '\n'
+#define MOD (int)(998244353)
+
+const int N = 1000005;
+const int M = N * 2;
+using namespace std;
+
+
+void solve()
+{
+
+    
+}
+
+signed main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    int T = 1;
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+    return 0;
+}
