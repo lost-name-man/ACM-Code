98f458e562804126cefe9ae30c3a8009598afc9b
lost-name-man <3014326408@qq.com>
2025-05-06 16:10:33 +0800

    10ver2

--- DingPa/8/10.cpp
+++ DingPa/8/10.cpp
@@ -26,62 +26,77 @@ const int N = 1000005;
 const int M = N * 2;
 using namespace std;
 
-
 void solve()
 {
 
-    int k,c,d,e,f;
-    cin>>k>>c>>d>>e>>f;
-    int ans=0;
+    int k, c, d, e, f;
+    cin >> k >> c >> d >> e >> f;
+    int ans = 0;
 
-    map<int, int>posb_a;
-    for(int bval=0; bval<=10006; bval++)
+    map<int, int> posb_a;
+    for (int bval = 0; bval <= 10006; bval++)
     {
-        int aval=(bval*bval*bval)*c+(bval*bval)*d+bval*e+f;
-        posb_a[aval]=bval;
+        int aval = (bval * bval * bval) * c + (bval * bval) * d + bval * e + f;
+        posb_a[aval] = bval;
     }
 
-    for(auto iter=posb_a.begin(); iter!=posb_a.end(); iter++)
+    if (c == 0 && d == 0 & e == 0)
     {
-        int nowa=iter->first;
-        int nowb=iter->second;
+        int ok = 1;
+        while (f)
+        {
+            if (f % 27 == 0)
+            {
+                ok = 0;
+            }
+            f /= 27;
+        }
+        if (ok == 0)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << 1 << endl;
+        }
+        return;
+    }
+    for (auto iter = posb_a.begin(); iter != posb_a.end(); iter++)
+    {
+        int nowa = iter->first;
+        int nowb = iter->second;
 
         vector<int> as;
-        int ok=1;
-        while(nowa)
+        int ok = 1;
+        while (nowa)
         {
-            if(nowa%27==0)
+            if (nowa % 27 == 0)
             {
-                ok=0;
+                ok = 0;
             }
-            as.push_back(nowa%27);
-            nowa/=27;
+            as.push_back(nowa % 27);
+            nowa /= 27;
         }
 
-        
-        if(ok==0||as.size()==0||as.size()>k)
+        if (ok == 0 || as.size() == 0 || as.size() > k)
         {
             continue;
         }
 
-        int bval=0;
-        int base=1;
-        for(int i=0;i<as.size();i++)
+        int bval = 0;
+        int base = 1;
+        for (int i = 0; i < as.size(); i++)
         {
-            bval+=as[i]*base;
-            bval%=10007;
-            base*=10;
+            bval += as[i] * base;
+            bval %= 10007;
+            base *= 10;
         }
-        if(bval==nowb)
+        if (bval == nowb)
         {
             ans++;
         }
-
-
     }
-    cout<<ans<<endl;
-
-
+    cout << ans << endl;
 }
 
 signed main()
