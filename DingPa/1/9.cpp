a7b9eb118cd77e525a3d1ff916ab3696cfb4ed25
BorderSu <1085626434@qq.com>
2025-05-16 16:21:00 +0800

    remidiation-1-9-start

---
+++ DingPa/1/9.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <iomanip>
+
+#define int long long
+#define INF 1e18
+#define endl '\n'
+#define MOD (int)(1000000007)
+
+using namespace std;
+
+void solve()
+{
+    int n, m;
+    cin>>n>>m;
+    vector<int>arr(n+5, 0);
+    for(int i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+
+    }
+    vector<int>grr(pow(2, m))
+
+}
+
+signed main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    int T = 1;
+    cin >> T;
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+    return 0;
+}
