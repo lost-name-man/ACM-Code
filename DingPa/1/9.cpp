219f49ef884987b1e5014969ffb71a658733eefb
lost-name-man <3014326408@qq.com>
2025-05-16 17:05:28 +0800

    1-9ver1

--- DingPa/1/9.cpp
+++ DingPa/1/9.cpp
@@ -24,18 +24,64 @@
 
 using namespace std;
 
+int n, m;
 void solve()
 {
-    int n, m;
-    cin>>n>>m;
-    vector<int>arr(n+5, 0);
-    for(int i=1; i<=n; i++)
+
+    cin >> n >> m;
+    int M = pow(2, m);
+    vector<int> arr(n + 5, 0);
+    for (int i = 1; i <= n; i++)
     {
-        cin>>arr[i];
-
+        cin >> arr[i];
     }
-    vector<int>grr(pow(2, m))
+    vector<int> grr(M + 5);
+    for (int i = 1; i <= M; i++)
+    {
+        cin >> grr[i - 1];
+    }
+    vector<int> dp(n + 5, 0);
+    vector<vector<int>> index_of_j(22, vector<int>(n + 5, 1));
 
+    vector<vector<int>> preein(22, vector<int>(n + 5, 0));
+    for (int i = 1; i <= 20; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            preein[i][j] = preein[i][j - 1] + ((arr[j] >> (i - 1)) & 1);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        int maxn = 0;
+        int nownum = arr[i];
+        dp[i] = dp[i - 1];
+        for (int j = 1; j <= 20; j++)
+        {
+            int nowzwei = (nownum >> (j - 1)) & 1;
+            int nowindex = index_of_j[j][nowzwei ^ 1];
+
+            index_of_j[j][nowzwei] = i;
+            if (nowindex == 0)
+            {
+                continue;
+            }
+            int len = i - nowindex + 1;
+            int nowf = 0;
+            for (int k = 1; k <= 20; k++)
+            {
+                int onenum = (preein[k][i] - preein[k][nowindex - 1]);
+                if (onenum != 0 && onenum != len)
+                {
+                    nowf |= (1 << (k - 1));
+                }
+            }
+            maxn = max(maxn, grr[nowf] + dp[nowindex - 1]);
+        }
+        dp[i] = max(dp[i], maxn);
+    }
+
+    cout << dp[n] << endl;
 }
 
 signed main()
