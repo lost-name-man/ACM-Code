0aa3668e9fbe7b0c734a90b66a4d1163e3da9ead
lost-name-man <3014326408@qq.com>
2025-05-17 19:12:45 +0800

    MERGE

---
+++ DingPa/10/9.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <iomanip>
+
+#define int long long
+#define INF 1e18
+#define endl '\n'
+#define MOD (int)(1000000007)
+
+using namespace std;
+
+int n, m;
+int arr[1000006] = {0};
+void solve()
+{
+    int ans = 0;
+    for (int i = 2; i <= 1e6; i++)
+    {
+        if (arr[i] == 0)
+        {
+            ans++;
+            for (int j = i; j <= 1e6; j += i)
+            {
+                arr[j] = 1;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    int T = 1;
+    cin >> T;
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+    return 0;
+}
