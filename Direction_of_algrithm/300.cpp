e166433f3713d88c6aee881e201a2485e7ed19ef
lost-name-man <3014326408@qq.com>
2025-07-08 17:25:57 +0800

    300-release

--- Direction_of_algrithm/300.cpp
+++ Direction_of_algrithm/300.cpp
@@ -7,8 +7,29 @@
 
 using namespace std;
 
+int n, s;
+int t[5003], c[5003];
+int dp[5003];
+int pret[5003], prec[5003];
 void solve()
 {
+    cin >> n >> s;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> t[i] >> c[i];
+        pret[i] = pret[i - 1] + t[i];
+        prec[i] = prec[i - 1] + c[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        int minn = INF;
+        for (int j = 0; j < i; j++)
+        {
+            minn = min(minn, dp[j] + s * (prec[n] - prec[j]) + (pret[i] * (prec[i] - prec[j])));
+        }
+        dp[i] = minn;
+    }
+    cout << dp[n] << endl;
 }
 
 signed main()
