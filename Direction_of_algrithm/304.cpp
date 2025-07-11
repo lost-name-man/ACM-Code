2647e19455d37cce35f301acca498297846e0919
lost-name-man <3014326408@qq.com>
2025-07-11 15:35:40 +0800

    304-release

--- Direction_of_algrithm/304.cpp
+++ Direction_of_algrithm/304.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+typedef long double ld;
 #define int long long
 #define INF 1e18
 #define endl '\n'
@@ -9,63 +10,140 @@ using namespace std;
 
 const int N = 100000;
 
-int n, m, p;
-int d[N + 10];
-int arr[N + 10];
-int prearr[N + 10];
-int dq[N + 12];
-int ll = 0, rr = -1;
-
-int f[102][N + 10];
-
-int y(int i, int k)
+struct Node
 {
-    return f[i - 1][k] + prearr[k];
+    int l;
+    int r;
+    int j;
+};
+
+int n, l, p;
+string poem[N + 10];
+int arr[N + 10];
+vector<int> prearr;
+vector<ld> f;
+vector<int> lastindex;
+Node dq[N + 10];
+int ll = 0, rr = 0;
+
+ld mypow(ld x)
+{
+    ld v = 1;
+    for (int k = p; k; k >>= 1, x *= x)
+        if (k & 1)
+            v *= x;
+    return (v > 0 ? v : -v);
+}
+ld fi(int i, int j)
+{
+    return f[j] + mypow(abs(l - (prearr[i] - prearr[j] + i - j - 1)));
 }
 void solve()
 {
-    memset(f, 0x7f, sizeof(f));
-    cin >> n >> m >> p;
-    for (int i = 2; i <= n; i++)
+    cin >> n >> l >> p;
+    prearr = vector<int>(n + 10);
+    f = vector<ld>(n + 10);
+    lastindex = vector<int>(n + 10);
+    ll = 0, rr = 0;
+    dq[0] = {1, n, 0};
+    for (int i = 1; i <= n; i++)
     {
-        cin >> d[i];
-        d[i] += d[i - 1];
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        int h, t;
-        cin >> h >> t;
-        arr[i] = t - d[h];
+        cin >> poem[i];
+        arr[i] = poem[i].size();
         prearr[i] = prearr[i - 1] + arr[i];
     }
 
-    sort(arr + 1, arr + 1 + m);
-    for (int i = 0; i <= p; i++)
+    for (int i = 1; i <= n; i++)
     {
-        f[i][0] = 0;
-    }
-    for (int i = 1; i <= p; i++)
-    {
-        ll = 0, rr = 0;
-        dq[0] = 0;
-        for (int j = 1; j <= m; j++)
+        while (ll <= rr && dq[ll].r < i)
         {
-            int nowtan = arr[j];
-            while (ll < rr && nowtan >= (double)(y(i, dq[ll + 1]) - y(i, dq[ll])) / (double)(dq[ll + 1] - dq[ll]))
+            ll++;
+        }
+        dq[ll].l = i;
+        int j = dq[ll].j;
+        f[i] = fi(i, j);
+        lastindex[i] = j;
+
+        int ok = 0;
+        while (ll <= rr && fi(dq[rr].l, dq[rr].j) >= fi(dq[rr].l, i))
+        {
+            ok = 1;
+            rr--;
+        }
+
+        if (ll <= rr && fi(dq[rr].r, dq[rr].j) >= fi(dq[rr].r, i))
+        {
+            ok = 1;
+            int l = dq[rr].l, r = dq[rr].r, j = dq[rr].j;
+            int mid;
+            int jindex;
+            while (l <= r)
             {
-                ll++;
+                mid = (l + r) / 2;
+                if (fi(mid, j) >= fi(mid, i))
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    jindex = mid;
+                    l = mid + 1;
+                }
             }
-            int k = dq[ll];
-            f[i][j] = f[i - 1][k] + arr[j] * (j - k) - (prearr[j] - prearr[k]);
-            while (ll < rr && (double)(y(i, j) - y(i, dq[rr])) / (double)(j - dq[rr]) <=
-                                  (double)(y(i, dq[rr]) - y(i, dq[rr - 1])) / (double)(dq[rr] - dq[rr - 1]))
+            dq[rr].r = jindex;
+            dq[++rr] = {jindex + 1, n, i};
+        }
+        else if (ok != 0)
+        {
+
+            int index;
+            if (ll <= rr)
             {
-                rr--;
+                index = dq[rr].r;
             }
-            dq[++rr] = j;
+            else
+            {
+                i + 1;
+            }
+            dq[++rr] = {index + 1, n, i};
         }
     }
-    cout << f[p][m] << endl;
+    int it = n;
+    vector<int> end_of_poem;
+    while (it != 0)
+    {
+        end_of_poem.push_back(it);
+        it = lastindex[it];
+    }
+
+    reverse(end_of_poem.begin(), end_of_poem.end());
+    it = 0;
+    if (f[n] <= INF)
+    {
+        cout << (int)f[n] << endl;
+        for (int i = 1; i <= n; i++)
+        {
+            cout << poem[i];
+            if (i != end_of_poem[it])
+            {
+                cout << ' ';
+            }
+            if (i == end_of_poem[it])
+            {
+                it++;
+                cout << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "Too hard to arrange" << endl;
+    }
+    for (int i = 1; i <= 20; i++)
+    {
+        cout << '-';
+    }
+    cout << endl;
 }
 
 signed main()
@@ -74,7 +152,7 @@ signed main()
     // std::cin.tie(0);
     int T = 1;
 
-    // cin >> T;
+    cin >> T;
     for (int i = 1; i <= T; i++)
     {
         solve();
