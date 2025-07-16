f3335bda1842868bcedb13f76f2b53579d886b20
lost-name-man <3014326408@qq.com>
2025-07-16 17:03:46 +0800

    L-ver1

---
+++ Nodecoder-2025-SummerCamp/l.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+typedef long double ld;
+#define int long long
+#define INF 1e18
+#define endl '\n'
+#define MOD 1000000000000000003
+
+using namespace std;
+
+int n, q;
+int arr[200005];
+void solve()
+{
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++)
+    {
+    }
+}
+
+signed main()
+{
+    // std::ios::sync_with_stdio(0);
+    // std::cin.tie(0);
+    int T = 1;
+
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+    return 0;
+}
