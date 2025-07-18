487c1c06074fc2d9d7930c33894c28f867ce3414
BorderSu <suqin1329@gmail.com>
2025-07-18 11:58:40 +0800

    dingpa2025

---
+++ DingPa2025/1/A.cpp
@@ -0,0 +1,31 @@
+#include <climits>
+#include <iostream>
+#include <map>
+#include <utility>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+#define int long long
+#define endl '\n'
+const long long INF = 1e18 + 7;
+
+
+int solve()
+{
+    
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    // cin>>T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+}
