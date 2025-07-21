b079b68a65da101efdb5d77009aac70b30551882
BorderSu <suqin1329@gmail.com>
2025-07-21 13:19:53 +0800

    1006-done

--- DingPa2025/2/1006.cpp
+++ DingPa2025/2/1006.cpp
@@ -19,15 +19,77 @@ int lowbit(int x)
     int ret=x&(-x);
     return ret;
 }
+
+int n;
+
 vector<int>tree;
 
-int sum(int )
+void adds(int x, int val)
+{
+    for(int i=x; i<=n; i+=lowbit(i))
+    {
+        tree[i]+=val;
+    }
+}
+
+int querys(int l, int r)
+{
+    int ret=0;
+    for(int i=r; i; i-=lowbit(i))
+    {
+        ret+=tree[i];
+    }
+    for(int i=l-1; i; i-=lowbit(i))
+    {
+        ret-=tree[i];
+    }
+
+    return ret;
+
+}
 
 
 
 void solve()
 {
+    cin>>n;
+    tree=vector<int>(n+5, 0);
+    vector<int>arr(n+5, 0);
+    vector<int>brr(n+5, 0);
+    vector<int>arr_index(n+5, 0);
+    vector<int>baned(n+5, 0);
+    for(int i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+        arr_index[arr[i]]=i;
+        adds(i, 1);
+    }
+    for(int i=1; i<=n; i++)
+    {
+        cin>>brr[i];
+    }
     
+
+    vector<int>totans(n+5, 0);
+
+    for(int i=1; i<=n; i++)
+    {
+        int ans=0;
+        ans+=i-1;
+        int bwin=brr[i];
+        int binaindex=arr_index[bwin];
+        ans+=querys(1, binaindex-1);
+
+        totans[bwin]=ans;
+
+        baned[bwin]=1;
+        adds(binaindex, -1);
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        cout<<totans[i]<<" ";
+    }
 }
 
 signed main()
