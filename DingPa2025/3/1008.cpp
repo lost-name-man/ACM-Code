278c1b6a3bcf6cce85f88fb417e571222c063a38
lost-name-man <3014326408@qq.com>
2025-07-25 16:53:48 +0800

    1008-ver2

--- DingPa2025/3/1008.cpp
+++ DingPa2025/3/1008.cpp
@@ -17,31 +17,10 @@ using namespace std;
 
 int n;
 vector<int> arr;
-void solve()
+vector<int> arr0(n + 5);
+int prun()
 {
-    cin >> n;
-    arr = vector<int>(n + 5);
-    vector<int> arr0(n + 5), arr1(n + 5);
-    for (int i = 1; i <= n; i++)
-    {
-        char ch;
-        cin >> ch;
-        arr[i] = ch - '0';
-        arr0[i] = arr[i];
-        arr1[i] = arr[i];
-    }
-
-    int ans0 = 0, ans1 = 0;
-    if (arr[1] == 0)
-    {
-        ans1++;
-    }
-    else
-    {
-        ans0++;
-    }
-    arr0[1] = 0;
-    arr1[1] = 1;
+    int ans = 0;
     for (int i = 2; i < n; i++)
     {
         if (arr0[i] != arr0[i - 1])
@@ -53,62 +32,24 @@ void solve()
             if (arr0[i + 1] == arr0[i])
             {
                 arr0[i] = arr0[i] ^ 1;
-                ans0++;
+                ans++;
             }
             else
             {
                 swap(arr0[i], arr0[i + 1]);
-                ans0++;
+                ans++;
             }
         }
     }
-    if (arr0[n] == 0)
+    if (arr0[n] == arr0[1])
     {
-        ans0++;
+        ans++;
     }
-
-    for (int i = 2; i < n; i++)
-    {
-        if (arr1[i] != arr1[i - 1])
-        {
-            continue;
-        }
-        else
-        {
-            if (arr1[i + 1] == arr1[i])
-            {
-                arr1[i] = arr1[i] ^ 1;
-                ans1++;
-            }
-            else
-            {
-                swap(arr1[i], arr1[i + 1]);
-                ans1++;
-            }
-        }
-    }
-    if (arr1[n] == 1)
-    {
-        ans1++;
-    }
-    ///////////////////////////////////////
-
-    for (int i = 1; i <= n; i++)
-    {
-        arr0[i] = arr[i];
-        arr1[i] = arr[i];
-    }
-    int ans00 = 0, ans11 = 0;
-    if (arr[1] == 0)
-    {
-        ans11++;
-    }
-    else
-    {
-        ans00++;
-    }
-    arr0[1] = 0;
-    arr1[1] = 1;
+    return ans;
+}
+int nrun()
+{
+    int ans = 0;
     for (int i = n; i > 2; i--)
     {
         int lastindex = i + 1;
@@ -125,50 +66,67 @@ void solve()
             if (arr0[i - 1] == arr0[i])
             {
                 arr0[i] = arr0[i] ^ 1;
-                ans00++;
+                ans++;
             }
             else
             {
                 swap(arr0[i], arr0[i - 1]);
-                ans00++;
+                ans++;
             }
         }
     }
-    if (arr0[2] == 0)
+    if (arr0[2] == arr0[1])
     {
-        ans00++;
+        ans++;
+    }
+    return ans;
+}
+void initarr0()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        arr0[i] = arr[i];
+    }
+}
+void solve()
+{
+    cin >> n;
+    arr = vector<int>(n + 5);
+
+    for (int i = 1; i <= n; i++)
+    {
+        char ch;
+        cin >> ch;
+        arr[i] = ch - '0';
+        arr0[i] = arr[i];
     }
 
-    for (int i = n; i > 2; i--)
-    {
-        int lastindex = i + 1;
-        if (i + 1 > n)
-        {
-            lastindex = 1;
-        }
-        if (arr1[i] != arr1[lastindex])
-        {
-            continue;
-        }
-        else
-        {
-            if (arr1[i - 1] == arr1[i])
-            {
-                arr1[i] = arr1[i] ^ 1;
-                ans11++;
-            }
-            else
-            {
-                swap(arr1[i], arr1[i - 1]);
-                ans11++;
-            }
-        }
-    }
-    if (arr1[2] == 1)
-    {
-        ans11++;
-    }
-    cout << min({ans0, ans1, ans00, ans11}) << endl;
+    int ans;
+    ans = prun();
+    initarr0();
+    ans = min(ans, nrun());
+
+    initarr0();
+    arr0[1] ^= 1;
+    ans = min(ans, prun() + 1);
+    initarr0();
+    arr0[1] ^= 1;
+    ans = min(ans, nrun() + 1);
+
+    initarr0();
+    swap(arr0[1], arr0[n]);
+    ans = min(ans, prun() + 1);
+    initarr0();
+    swap(arr0[1], arr0[n]);
+    ans = min(ans, nrun() + 1);
+
+    initarr0();
+    swap(arr0[1], arr0[2]);
+    ans = min(ans, prun() + 1);
+    initarr0();
+    swap(arr0[1], arr0[2]);
+    ans = min(ans, nrun() + 1);
+    cout << ans << endl;
 }
 
 signed main()
