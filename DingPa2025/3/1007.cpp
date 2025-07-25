3c0f7ca056c286eb71de8b2e0c0e5471974d54b4
lost-name-man <3014326408@qq.com>
2025-07-25 13:24:48 +0800

    1007-ver1

--- DingPa2025/3/1007.cpp
+++ DingPa2025/3/1007.cpp
@@ -2,61 +2,83 @@
 #include <map>
 #include <vector>
 #include <queue>
+#include <algorithm>
+#include <set>
 using namespace std;
 #define int long long
 #define endl '\n'
 
 const int INF = 2e9 + 7;
 
-
 struct Inval
 {
     int l;
     int r;
-    
-    bool operator <(const Inval &other) const
+
+    bool operator<(const Inval &other) const
     {
-        if(this->l < other.l)
+        if (this->r == other.r)
         {
-            return this->r > other.r;
+            return this->l < other.l;
         }
-        return this->l < other.l;
+        return this->r < other.r;
     }
-
-
 };
 
 void solve()
 {
     int n;
-    cin>>n;
-    vector<Inval>arr(n+5);
-    for(int i=1; i<=n; i++)
-    {   
-        cin>>arr[i].l>>arr[i].r;
-    }
-
-    sort(arr.begin()+1, arr.begin()+1+n);
-
-
-    arr[0].l=-1;
-    arr[0].r=-1;
-    int ans=1;
-    int 
-    for(int i=1; i<=n; i++)
+    cin >> n;
+    vector<Inval> arr(n + 5);
+    for (int i = 1; i <= n; i++)
     {
-        int nowl=arr[i].l;
-        int nowr=arr[i].r;
-        int lastl=arr[i-1].l;
-        int lastr=arr[i-1].r;
-        
-
+        cin >> arr[i].l >> arr[i].r;
     }
 
-    cout<<ans<<endl;
+    sort(arr.begin() + 1, arr.begin() + 1 + n);
 
+    map<int, int> mp;
+    set<int> st;
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int nowl = arr[i].l, nowr = arr[i].r;
+        if (mp[nowr] == 0)
+        {
+            ans++;
+        }
+        auto it = st.lower_bound(nowl);
+        int nextl;
+        if (it == st.end())
+        {
+            mp[nowl] = 1;
+            if (mp[nowr] != 1)
+            {
+                mp[nowr] = 2;
+            }
+            st.insert(nowl);
+            st.insert(nowr);
 
-
+            continue;
+        }
+        else
+        {
+            nextl = *it;
+            if (mp[nextl] == 2)
+            {
+                ans++;
+            }
+        }
+        mp[nowl] = 1;
+        if (mp[nowr] != 1)
+        {
+            mp[nowr] = 2;
+        }
+        st.insert(nowl);
+        st.insert(nowr);
+    }
+    ans++;
+    cout << ans << endl;
 }
 
 signed main()
@@ -66,7 +88,7 @@ signed main()
 
     int T = 1;
 
-    cin>>T;
+    cin >> T;
 
     for (int i = 1; i <= T; i++)
     {
