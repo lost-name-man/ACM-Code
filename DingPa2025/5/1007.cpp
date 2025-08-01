ede0bf5c046b3c210d20c18fa04c441eefd2db31
lost-name-man <3014326408@qq.com>
2025-08-01 14:53:59 +0800

    1007-ver2

--- DingPa2025/5/1007.cpp
+++ DingPa2025/5/1007.cpp
@@ -24,45 +24,56 @@ using namespace std;
 #define endl '\n'
 static mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
 const int INF = 1e16 + 7;
-const int MOD = 1e9+7;
-
+const int MOD = 1e9 + 7;
 
 struct Node
 {
     int son, mu;
 };
 
-int qpow(int base, int p)
+int qpow(int n, int p)
 {
-    
+    int base = n, re = 1;
+    while (p)
+    {
+        if (p & 1)
+        {
+            re *= base;
+            re %= MOD;
+        }
+        base *= base;
+        base %= MOD;
+        p >>= 1;
+    }
+    return re;
 }
 
+int inv(int x)
+{
+    return qpow(x, MOD - 2);
+}
 
 Node cxx(int down, int up)
 {
-    int u=1;
-    for(int i=down; i>down-up; i--)
+    int u = 1;
+    for (int i = down; i > down - up; i--)
     {
-        u*=i;
-        u%=MOD;
+        u *= i;
+        u %= MOD;
     }
 
-    int d=1;
-    for(int i=1; i<=up; i++)
+    int d = 1;
+    for (int i = 1; i <= up; i++)
     {
-        d*=i;
-        d%=MOD;
+        d *= i;
+        d %= MOD;
     }
-
 
     return {u, d};
-
 }
 
 void solve()
 {
-
-    
 
     int n, k;
     cin >> n >> k;
