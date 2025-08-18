7280faf5e3f005038d118bfce710aa472a3c7f58
BorderSu <suqin1329@gmail.com>
2025-08-18 12:09:54 +0800

    HD2025-1013-done

---
+++ DingPa2025/10/1013.cpp
@@ -0,0 +1,54 @@
+#include <cstddef>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <iomanip>
+#include <random>
+#include <chrono>
+using namespace std;
+#define int long long
+#define endl '\n'
+static mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+const int INF = 1e17 + 7;
+const int MOD = 1e17 + 7;
+
+
+
+void solve()
+{
+    int n, m;
+    cin>>n>>m;
+    int minn=min(n, m);
+    int maxn=max(n, m);
+    int ans=minn*maxn;
+    cout<<ans<<endl;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        solve();
+    }
+}
