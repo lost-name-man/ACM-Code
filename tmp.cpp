1001432da55c96144e62a2ae920ec98f47dff29f
lost-name-man <3014326408@qq.com>
2025-09-10 19:27:09 +0800

    tmp

--- tmp.cpp
+++ tmp.cpp
@@ -30,7 +30,22 @@ void solve()
         mp['A' + i] = i + 36;
     }
 
-    for (int i = 1; i <= n; i++)
+    if (s[1] >= 'A' && s[1] <= 'Z')
+    {
+        int nowc = mp[s[1]];
+        f[1][nowc][1] = 1;
+    }
+    if (s[1] >= '0' && s[1] <= '9')
+    {
+        int nowc = mp[s[1]];
+        f[1][nowc][(1 << 2)] = 1;
+    }
+    if (s[1] >= 'a' && s[1] <= 'z')
+    {
+        int nowc = mp[s[1]];
+        f[1][nowc][(1 << 1)] = 1;
+    }
+    for (int i = 2; i <= n; i++)
     {
         int nowc = mp[s[i]];
         if (s[i] >= 'A' && s[i] <= 'Z')
@@ -61,27 +76,136 @@ void solve()
             for (int st = 0; st < 8; st++)
             {
                 int sum0 = 0, sum1 = 0;
+
+                if ((st & (1 << 2)) == 0)
+                {
+                    continue;
+                }
                 for (int j = 0; j < 62; j++)
                 {
                     if (nowc == j)
                     {
                         continue;
                     }
-                    sum0 += f[i - 1][j][st];
-                    sum1 += f[i - 1][j][st ^ 1];
+                    sum1 += f[i - 1][j][st];
+                    sum0 += f[i - 1][j][st ^ (1 << 2)];
                 }
+                f[i][nowc][st] += sum0 + sum1;
+            }
+        }
+
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            int tmpc = nowc + 26;
+            for (int st = 0; st < 8; st++)
+            {
+                int sum0 = 0, sum1 = 0;
+
                 if ((st & 1) == 0)
                 {
                     continue;
                 }
-                else
+                for (int j = 0; j < 62; j++)
+                {
+                    if (tmpc == j)
+                    {
+                        continue;
+                    }
+                    sum1 += f[i - 1][j][st];
+                    sum0 += f[i - 1][j][st ^ 1];
+                }
+                f[i][tmpc][st] += sum0 + sum1;
+            }
+
+            for (int st = 0; st < 8; st++)
+            {
+                int sum0 = 0, sum1 = 0;
+
+                if ((st & (1 << 1)) == 0)
+                {
+                    continue;
+                }
+                for (int j = 0; j < 62; j++)
+                {
+                    if (nowc == j)
+                    {
+                        continue;
+                    }
+                    sum1 += f[i - 1][j][st];
+                    sum0 += f[i - 1][j][st ^ (1 << 1)];
+                }
+                f[i][nowc][st] += sum0 + sum1;
+            }
+        }
+
+        if (s[i] == '?')
+        {
+            for (int st = 0; st < 8; st++) // shuzi
+            {
+                int all0 = 0, all1 = 0;
+
+                if ((st & (1 << 2)) == 0)
+                {
+                    continue;
+                }
+                for (int j = 0; j < 62; j++)
+                {
+                    all1 += f[i - 1][j][st];
+                    all0 += f[i - 1][j][st ^ (1 << 2)];
+                }
+                for (int tmpc = 0; tmpc <= 9; tmpc++)
+                {
+                    f[i][tmpc][st] = all1 - f[i - 1][tmpc][st] + all0;
+                }
+            }
+            for (int st = 0; st < 8; st++) // xiao xie
+            {
+                int all0 = 0, all1 = 0;
+
+                if ((st & (1 << 1)) == 0)
+                {
+                    continue;
+                }
+                for (int j = 0; j < 62; j++)
                 {
 
-                    f[i][nowc][st] += sum0 + sum1;
+                    all1 += f[i - 1][j][st];
+                    all0 += f[i - 1][j][st ^ (1 << 1)];
+                }
+                for (int tmpc = 10; tmpc <= 35; tmpc++)
+                {
+                    f[i][tmpc][st] = all1 - f[i - 1][tmpc][st] + all0;
+                }
+            }
+
+            for (int st = 0; st < 8; st++) // daxie
+            {
+                int all0 = 0, all1 = 0;
+
+                if ((st & 1) == 0)
+                {
+                    continue;
+                }
+                for (int j = 0; j < 62; j++)
+                {
+
+                    all1 += f[i - 1][j][st];
+                    all0 += f[i - 1][j][st ^ 1];
+                }
+                for (int tmpc = 36; tmpc <= 61; tmpc++)
+                {
+                    f[i][tmpc][st] = all1 - f[i - 1][tmpc][st] + all0;
                 }
             }
         }
     }
+
+    int ans = 0;
+    for (int i = 0; i < 62; i++)
+    {
+        ans += f[n][i][7];
+    }
+    cout << ans << endl;
 }
 
 signed main()
@@ -89,7 +213,7 @@ signed main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     for (int i = 1; i <= T; i++)
     {
         solve();
