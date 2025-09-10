c7d5163859ccff8e46ff5fcbad3cd9696b2fd977
lost-name-man <3014326408@qq.com>
2025-09-10 22:54:19 +0800

    tmp

--- tmp.cpp
+++ tmp.cpp
@@ -6,249 +6,243 @@ const int INF = 1e18;
 const int MOD = 998244353;
 
 int n;
-string s;
 
-map<char, int> mp;
+struct Edge
+{
+    int from;
+    int to;
+    int nex;
+};
+vector<int> head;
+vector<Edge> edge;
 
-int f[2][64][8];
+int tot = 1;
+void add(int x, int y)
+{
+    tot++;
+    edge[tot].from = x;
+    edge[tot].to = y;
+    edge[tot].nex = head[x];
+    head[x] = tot;
+}
 
+vector<int> fa;
+vector<int> nodenum;
+
+int findfa(int x)
+{
+    if (fa[x] == x)
+    {
+        return x;
+    }
+    fa[x] = findfa(fa[x]);
+    return fa[x];
+}
+void connect(int x, int y)
+{
+    int fax = findfa(x), fay = findfa(y);
+    if (fax == fay)
+    {
+        return;
+    }
+
+    fa[fax] = fay;
+    return;
+}
+
+int mypow(int n, int p)
+{
+    int base = n, re = 1;
+    while (p)
+    {
+        if (p & 1)
+        {
+            re *= base;
+            re %= MOD;
+        }
+        base *= base;
+        base %= MOD;
+        p >>= 1;
+    }
+    return re;
+}
+
+int inv(int x)
+{
+    return mypow(x, MOD - 2);
+}
+
+vector<vector<int>> lfa;
+vector<int> deep;
+void initlca()
+{
+    vector<int> vis(n + 1, 0);
+    deep = vector<int>(n + 5);
+    queue<int> q;
+    q.push(1);
+    vis[1] = 1;
+    deep[1] = 1;
+    while (!q.empty())
+    {
+        int nownode = q.front();
+        q.pop();
+
+        for (int i = head[nownode]; i; i = edge[i].nex)
+        {
+            int nextnode = edge[i].to;
+            if (vis[nextnode])
+            {
+                continue;
+            }
+            lfa[nextnode][0] = nownode;
+            for (int i = 1; i <= 20; i++)
+            {
+                lfa[nextnode][i] = lfa[lfa[nextnode][i - 1]][i - 1];
+            }
+            q.push(nextnode);
+            vis[nextnode] = 1;
+            deep[nextnode] = deep[nownode] + 1;
+        }
+    }
+}
+
+int lca(int x, int y)
+{
+    if (deep[x] < deep[y])
+    {
+        swap(x, y);
+    }
+
+    while (deep[x] > deep[y])
+    {
+        for (int i = 20; i >= 0; i--)
+        {
+            int fax = lfa[x][i];
+            if (deep[fax] >= deep[y])
+            {
+                x = fax;
+            }
+        }
+    }
+
+    if (x == y)
+    {
+        return y;
+    }
+    else
+    {
+        for (int i = 20; i >= 0; i--)
+        {
+            int fax = lfa[x][i];
+            int fay = lfa[y][i];
+            if (fax != fay)
+            {
+                x = fax;
+                y = fay;
+            }
+        }
+        return lfa[x][0];
+    }
+}
+int checkin(int x, int y)
+{
+    int xylca = lca(x, y);
+    int lcafa = findfa(xylca);
+    if (lcafa == x)
+    {
+        return 1;
+    }
+    else if (lcafa == y)
+    {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+vector<Edge> err;
 void solve()
 {
     cin >> n;
-    cin >> s;
-    s = '0' + s;
-    for (int i = 0; i < 9; i++)
+    fa = nodenum = head = vector<int>(n + 5);
+    edge = vector<Edge>(n * 2 + 5);
+    err = vector<Edge>(n + 5);
+
+    lfa = vector<vector<int>>(n + 5, vector<int>(24));
+    tot = 1;
+
+    for (int i = 1; i <= n; i++)
     {
-        mp['0' + i] = i;
+        fa[i] = i;
+        nodenum[i] = 1;
     }
-    for (int i = 0; i < 26; i++)
+    for (int i = 1; i <= n - 1; i++)
     {
-        mp['a' + i] = i + 10;
-    }
-    for (int i = 0; i < 26; i++)
-    {
-        mp['A' + i] = i + 36;
+        cin >> err[i].from >> err[i].to;
     }
 
-    if (s[1] >= 'A' && s[1] <= 'Z')
+    for (int i = 1; i <= n - 1; i++)
     {
-        int nowc = mp[s[1]];
-        f[1][nowc][1] = 1;
+        int x, y;
+        cin >> x >> y;
+        add(x, y);
+        add(y, x);
     }
-    if (s[1] >= '0' && s[1] <= '9')
+
+    initlca();
+    // cout << "!" << endl;
+
+    int ans = 1;
+    for (int i = 1; i <= n - 1; i++)
     {
-        int nowc = mp[s[1]];
-        f[1][nowc][(1 << 2)] = 1;
-    }
-    if (s[1] >= 'a' && s[1] <= 'z')
-    {
-        int nowc = mp[s[1]];
-        f[1][nowc][(1 << 1)] = 1;
-        f[1][nowc + 26][1] = 1;
-    }
-    if (s[1] == '?')
-    {
-        for (int i = 0; i <= 9; i++)
+        int x = err[i].from, y = err[i].to;
+        int fax = findfa(x), fay = findfa(y);
+
+        int checkout = checkin(fax, fay);
+        if (checkout == 0)
         {
-            f[1][i][(1 << 2)] = 1;
+            ans = 0;
+            break;
         }
-        for (int i = 10; i <= 35; i++)
+        else if (checkout == 2)
         {
-            f[1][i][(1 << 1)] = 1;
-        }
-        for (int i = 36; i <= 61; i++)
-        {
-            f[1][i][1] = 1;
-        }
-    }
-    for (int i = 2; i <= n; i++)
-    {
-        for (int j = 0; j < 62; j++)
-        {
-            for (int st = 0; st < 8; st++)
+            int tmpnode = fax;
+            for (int i = 20; i >= 0; i--)
             {
-                f[i & 1][j][st] = 0;
+                int fatmp = lfa[tmpnode][i];
+                if (findfa(fatmp) == fax)
+                {
+                    tmpnode = fatmp;
+                }
+            }
+            if (findfa(lfa[tmpnode][0]) == fay)
+            {
+                ans *= inv(nodenum[fax] * nodenum[fay]);
+                ans %= MOD;
             }
         }
-        int nowc = mp[s[i]];
-        if (s[i] >= 'A' && s[i] <= 'Z')
+        else
         {
-            for (int st = 0; st < 8; st++)
+            int tmpnode = fay;
+            for (int i = 20; i >= 0; i--)
             {
-                int sum0 = 0, sum1 = 0;
-
-                if ((st & 1) == 0)
+                int fatmp = lfa[tmpnode][i];
+                if (findfa(fatmp) == fay)
                 {
-                    continue;
+                    tmpnode = fatmp;
                 }
-                for (int j = 0; j < 62; j++)
-                {
-                    if (nowc == j)
-                    {
-                        continue;
-                    }
-                    sum1 += f[(i - 1) & 1][j][st];
-                    sum1 %= MOD;
-                    sum0 += f[(i - 1) & 1][j][st ^ 1];
-                    sum0 %= MOD;
-                }
-                f[i & 1][nowc][st] = sum0 + sum1;
-                f[i & 1][nowc][st] %= MOD;
+            }
+            if (findfa(lfa[tmpnode][0]) == fax)
+            {
+                ans *= inv(nodenum[fax] * nodenum[fay]);
+                ans %= MOD;
             }
         }
 
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            for (int st = 0; st < 8; st++)
-            {
-                int sum0 = 0, sum1 = 0;
-
-                if ((st & (1 << 2)) == 0)
-                {
-                    continue;
-                }
-                for (int j = 0; j < 62; j++)
-                {
-                    if (nowc == j)
-                    {
-                        continue;
-                    }
-                    sum1 += f[(i - 1) & 1][j][st];
-                    sum0 += f[(i - 1) & 1][j][st ^ (1 << 2)];
-                    sum1 %= MOD;
-                    sum0 %= MOD;
-                }
-                f[i & 1][nowc][st] = sum0 + sum1;
-                f[i & 1][nowc][st] %= MOD;
-            }
-        }
-
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            int tmpc = nowc + 26;
-            for (int st = 0; st < 8; st++)
-            {
-                int sum0 = 0, sum1 = 0;
-
-                if ((st & 1) == 0)
-                {
-                    continue;
-                }
-                for (int j = 0; j < 62; j++)
-                {
-                    if (tmpc == j)
-                    {
-                        continue;
-                    }
-                    sum1 += f[(i - 1) & 1][j][st];
-                    sum0 += f[(i - 1) & 1][j][st ^ 1];
-                    sum1 %= MOD;
-                    sum0 %= MOD;
-                }
-                f[i & 1][tmpc][st] = sum0 + sum1;
-                f[i & 1][tmpc][st] %= MOD;
-            }
-
-            for (int st = 0; st < 8; st++)
-            {
-                int sum0 = 0, sum1 = 0;
-
-                if ((st & (1 << 1)) == 0)
-                {
-                    continue;
-                }
-                for (int j = 0; j < 62; j++)
-                {
-                    if (nowc == j)
-                    {
-                        continue;
-                    }
-                    sum1 += f[(i - 1) & 1][j][st];
-                    sum0 += f[(i - 1) & 1][j][st ^ (1 << 1)];
-                    sum1 %= MOD;
-                    sum0 %= MOD;
-                }
-                f[i & 1][nowc][st] = sum0 + sum1;
-                f[i & 1][nowc][st] %= MOD;
-            }
-        }
-
-        if (s[i] == '?')
-        {
-            for (int st = 0; st < 8; st++) // shuzi
-            {
-                int all0 = 0, all1 = 0;
-
-                if ((st & (1 << 2)) == 0)
-                {
-                    continue;
-                }
-                for (int j = 0; j < 62; j++)
-                {
-                    all1 += f[(i - 1) & 1][j][st];
-                    all0 += f[(i - 1) & 1][j][st ^ (1 << 2)];
-                    all1 %= MOD;
-                    all0 %= MOD;
-                }
-                for (int tmpc = 0; tmpc <= 9; tmpc++)
-                {
-                    f[i & 1][tmpc][st] = all1 - f[(i - 1) & 1][tmpc][st] + all0;
-                    f[i & 1][tmpc][st] = (f[i & 1][tmpc][st] % MOD + MOD) % MOD;
-                }
-            }
-            for (int st = 0; st < 8; st++) // xiao xie
-            {
-                int all0 = 0, all1 = 0;
-
-                if ((st & (1 << 1)) == 0)
-                {
-                    continue;
-                }
-                for (int j = 0; j < 62; j++)
-                {
-
-                    all1 += f[(i - 1) & 1][j][st];
-                    all0 += f[(i - 1) & 1][j][st ^ (1 << 1)];
-                    all1 %= MOD;
-                    all0 %= MOD;
-                }
-                for (int tmpc = 10; tmpc <= 35; tmpc++)
-                {
-                    f[i & 1][tmpc][st] = all1 - f[(i - 1) & 1][tmpc][st] + all0;
-                    f[i & 1][tmpc][st] = (f[i & 1][tmpc][st] % MOD + MOD) % MOD;
-                }
-            }
-
-            for (int st = 0; st < 8; st++) // daxie
-            {
-                int all0 = 0, all1 = 0;
-
-                if ((st & 1) == 0)
-                {
-                    continue;
-                }
-                for (int j = 0; j < 62; j++)
-                {
-
-                    all1 += f[(i - 1) & 1][j][st];
-                    all0 += f[(i - 1) & 1][j][st ^ 1];
-                    all1 %= MOD;
-                    all0 %= MOD;
-                }
-                for (int tmpc = 36; tmpc <= 61; tmpc++)
-                {
-                    f[i & 1][tmpc][st] = all1 - f[(i - 1) & 1][tmpc][st] + all0;
-                    f[i & 1][tmpc][st] = (f[i & 1][tmpc][st] % MOD + MOD) % MOD;
-                }
-            }
-        }
-    }
-
-    int ans = 0;
-    for (int i = 0; i < 62; i++)
-    {
-        ans += f[n & 1][i][7];
-        ans %= MOD;
+        nodenum[fa[y]] += nodenum[fa[x]];
+        connect(x, y);
     }
     cout << ans << endl;
 }
