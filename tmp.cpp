457e59390f6651212ff9304f1e882ba83605eb91
lost-name-man <3014326408@qq.com>
2025-09-18 16:00:30 +0800

    tmp

--- tmp.cpp
+++ tmp.cpp
@@ -5,189 +5,133 @@ using namespace std;
 const int INF = 1e18 + 3;
 const int MOD = 20220911;
 
-int l, r;
-vector<int> lbi, rbi;
-
-void tobi(int x, vector<int> &re)
+struct Node
 {
-    for (int i = 60; i >= 0; i--)
+    int l;
+    int r;
+    int val;
+    const bool operator<(const Node &a) const
     {
-        re[i] = (x >> i) & 1;
+        return this->l < a.l;
     }
-}
+};
+int n, q;
 
-int cmp(vector<int> &a, vector<int> &b)
-{
-    for (int i = 63; i >= 0; i--)
-    {
-        if (a[i] > b[i])
-        {
-            return 1;
-        }
-        else if (a[i] < b[i])
-        {
-            return -1;
-        }
-    }
-    return 0;
-}
 void solve()
 {
-    cin >> l >> r;
-    lbi = rbi = vector<int>(64);
-    tobi(l, lbi);
-    tobi(r, rbi);
+    vector<int> arr, pre0, pre1;
+    cin >> n >> q;
+    arr = vector<int>(n + 5);
+    pre0 = vector<int>(n + 5);
+    string str;
+    cin >> str;
 
-    vector<int> tmplbi(64);
-    vector<int> ansbi(64);
-
-    if (2 >= l && 2 <= r)
+    for (int i = 1; i <= n; i++)
     {
-        cout << 2 << endl;
-        return;
+        arr[i] = str[i - 1] - '0';
+        pre0[i] = pre0[i - 1] + (arr[i] == 0);
     }
-    int ok = 0;
-    for (int i = 2; i <= 32; i++)
+
+    vector<Node> block1;
+    block1.push_back({0, 0, 0});
+    pre1.push_back(0);
+    for (int i = 1; i <= n; i++)
     {
-        ansbi = vector<int>(64);
-        tmplbi = vector<int>(64);
-        int onenum = i - 1;
-        int low = (1 << i);
-        int tmpl = l - low;
-        tobi(tmpl, tmplbi);
-
-        int all0 = 1;
-        for (int j = 0; j <= i; j++)
+        if (arr[i] == 1)
         {
-            if (tmplbi[j] == 1)
+            int l = i, r = n;
+            for (int j = i; j <= n; j++)
             {
-                all0 = 0;
-            }
-        }
-        for (int j = 60; j > i; j--)
-        {
-            if (tmplbi[j] == 1)
-            {
-                if (onenum > 0)
+                if (arr[j] == 0)
                 {
-                    ansbi[j] = 1;
-                }
-                onenum--;
-            }
-        }
-        if (onenum < 0)
-        {
-            int chal;
-            for (int j = 0; j <= 60; j++)
-            {
-                if (ansbi[j] == 1 && ansbi[j + 1] == 0) // 1 to rear
-                {
-                    swap(ansbi[j], ansbi[j + 1]);
-                    chal = j + 1;
+                    r = j - 1;
                     break;
                 }
             }
-            int einnum = 0;
-            for (int j = i + 1; j <= chal - 1; j++)
+            int len = r - l + 1;
+            int nowval;
+            if (len & 1)
             {
-                if (ansbi[j] == 1)
-                {
-                    einnum++;
-                    ansbi[j] == 0;
-                }
+                nowval = len / 2 + 2;
+            }
+            else
+            {
+                nowval = len / 2;
             }
 
-            for (int j = i + 1; j <= chal - 1; j++)
-            {
-                if (einnum == 0)
-                {
-                    break;
-                }
-                if (ansbi[j] == 0)
-                {
-                    ansbi[j] = 1;
-                    einnum--;
-                }
-            }
+            block1.push_back({l, r, nowval});
+            pre1.push_back(pre1[pre1.size() - 1] + nowval);
+            i = r;
         }
-        else if (onenum > 0)
+    }
+
+    block1.push_back({n + 1, n + 1, 0});
+    pre1.push_back(pre1[pre1.size() - 1]);
+
+    for (int i = 1; i <= q; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        Node tmpl = {l, l}, tmpr = {r, r};
+        int itl = upper_bound(block1.begin(), block1.end(), tmpl) - block1.begin() - 1;
+        int itr = upper_bound(block1.begin(), block1.end(), tmpr) - block1.begin() - 1;
+
+        int preitl = itl, preitr = itr;
+
+        if (arr[r] == 1)
         {
-            for (int j = i + 1; j <= 63; j++)
-            {
-                if (ansbi[j] == 0)
-                {
-                    ansbi[j] = 1;
-                    onenum--;
-                }
-                if (onenum == 0)
-                {
-                    break;
-                }
-            }
+            preitr--;
+        }
+
+        preitl++;
+
+        int nowval = 0;
+        int nowlen;
+        if (arr[l] == 0 && arr[r] == 0)
+        {
+            nowlen = 0;
+        }
+        else if (arr[l] == 1 && arr[r] == 0)
+        {
+            int bl = l, br = block1[itl].r;
+            nowlen = (br - bl + 1);
+        }
+        else if (arr[l] == 0 && arr[r] == 1)
+        {
+            int bl = block1[itr].l, br = r;
+            nowlen = (br - bl + 1);
         }
         else
         {
-            if (all0 != 1)
-            {
-
-                //
-                int chal;
-                for (int j = 0; j <= 60; j++)
-                {
-                    if (ansbi[j] == 1 && ansbi[j + 1] == 0) // 1 to rear
-                    {
-                        swap(ansbi[j], ansbi[j + 1]);
-                        chal = j + 1;
-                        break;
-                    }
-                }
-                int einnum = 0;
-                for (int j = i + 1; j <= chal - 1; j++)
-                {
-                    if (ansbi[j] == 1)
-                    {
-                        einnum++;
-                        ansbi[j] == 0;
-                    }
-                }
-
-                for (int j = i + 1; j <= chal - 1; j++)
-                {
-                    if (einnum == 0)
-                    {
-                        break;
-                    }
-                    if (ansbi[j] == 0)
-                    {
-                        ansbi[j] = 1;
-                        einnum--;
-                    }
-                }
-            }
+            nowlen = (block1[itl].r - l + 1) + (r - block1[itr].l + 1);
         }
 
-        ansbi[i] = 1;
-        int check = cmp(ansbi, rbi);
-        if (check == 0 || check == -1)
+        if (nowlen & 1)
         {
-            ok = 1;
-            break;
+            nowval = nowlen / 2 + 2;
         }
-    }
+        else
+        {
+            nowval = nowlen / 2;
+        }
 
-    if (ok == 0)
-    {
-        cout << -1 << endl;
-        return;
-    }
+        if (preitr >= preitr)
+        {
+            nowval += pre1[preitr] - pre1[preitl - 1];
+        }
 
-    int ans = 0;
-    for (int i = 0; i <= 63; i++)
-    {
-        ans += ansbi[i] * (1 << i);
+        int now0 = pre0[r] - pre0[l - 1];
+        int ans;
+        if (nowval >= now0)
+        {
+            ans = 0;
+        }
+        else
+        {
+            ans = (now0 - nowval) / 3;
+        }
+        cout << ans << endl;
     }
-
-    cout << ans << endl;
 }
 
 signed main()
@@ -195,7 +139,7 @@ signed main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     for (int i = 1; i <= T; i++)
     {
         solve();
