ea61e92783a4f346d85126d9fb534fa8ea2c381b
BorderSu <suqin1329@gmail.com>
2025-11-02 16:13:12 +0800

    Hong Kong 2024

--- Liu-tmp.cpp
+++ Liu-tmp.cpp
@@ -1,90 +1,158 @@
 #include <iostream>
-#include <map>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
 #include <string>
+#include <cstdlib>
 #include <vector>
+#include <map>
+#include <list>
 #include <queue>
-
+#include <deque>
+#include <stack>
+#include <set>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <iomanip>
 using namespace std;
 #define int long long
 #define endl '\n'
 const int INF = 1e18 + 3;
-const int MOD = 20220911;
+const int MOD = 998244353;
 
 
-
+struct Nex
+{
+    int id;
+    int nexL=-1;
+    int nexR=-1;
+    int nexLR=-1;
+};
 
 void solve()
 {
-    int a, b;
-    cin>>a>>b;
 
-    // cout<<"!"<<a<<" "<<b<<endl;
+    string s;
+    cin>>s;
 
-    if(a==1)
-    {
-        cout<<1<<" "<<1<<endl;
-        return;
-    }
-    else if(b==1)
-    {
-        cout<<1<<" "<<0<<endl;
-        return;
-    }
+    s="@"+s + "@";
 
-    if(a%2==1)
-    {
-        cout<<1<<" "<<1<<endl;
-        return;
-    }
-    else if(b%2==1)
-    {
-        cout<<1<<" "<<0<<endl;
-        return;
-    }
 
-    if(a>b)
-    {   
-        int zweipow=0;
+    vector<Nex>nex(s.size()+5);
+
+
+
+    for(int i=s.size()-1; i>=0; i--)
+    {
+        if(i!=0)
         {
-            int tmpb=b;
-            while(tmpb%2==0)
+            nex[i-1].nexL=nex[i].nexL;
+            nex[i-1].nexR=nex[i].nexR;
+            nex[i-1].nexLR=nex[i].nexLR;
+        }
+        
+        if(s[i]=='L')
+        {
+            nex[i-1].nexL=i;
+            if(s[i+1]=='R')
             {
-                zweipow++;
-                tmpb/=2;
+                nex[i-1].nexLR=i;
             }
         }
-
-        // cout<<"!"<<zweipow<<" "<<0<<endl;
-        cout<<(zweipow+3)/2<<" "<<0<<endl;
-
-    }
-    else
-    {
-        int zweipow=0;
+        else if(s[i]=='R')
         {
-            int tmpb=a;
-            while(tmpb%2==0)
-            {
-                zweipow++;
-                tmpb/=2;
-            }
+            nex[i-1].nexR=i;
+        }
+        
+    }
 
+
+
+    int q;
+    cin>>q;
+    for(int Q=1; Q<=q; Q++)
+    {
+        string t;
+        cin>>t;
+
+        t=t+"@";
+
+        int si=0;
+        for(int ti=0; ti<t.size()-1; ti++)
+        {
+            char tch=t[ti];
+
+            if(tch=='L')
+            {
+                if(t[ti+1]=='R')
+                {
+                    int nexsi=nex[si].nexLR;
+                    if(nexsi==-1)
+                    {
+                        goto WAAA;
+                    }
+                    else
+                    {
+                        si=nexsi+1;
+                    }
+                }
+                else
+                {
+                    int nexsi=nex[si].nexL;
+                    if(nexsi==-1)
+                    {
+                        goto WAAA;
+                    }
+                    else
+                    {
+                        si=nexsi;
+                    }
+                }
+            }
+            else
+            {
+                int nexsi=nex[si].nexR;
+                if(nexsi==-1)
+                {
+                    goto WAAA;
+                }
+                else
+                {
+                    si=nexsi;
+                }
+            }
 
         }
 
-        cout<<(zweipow+2)/2<<" "<<1<<endl;
+
+        cout<<"YES"<<endl;
+
+
+        if(0)
+        {
+            WAAA:;
+
+            cout<<"NO"<<endl;
+
+        }
     }
-    
+
+
+
 }
-
-
 
 signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
     int T = 1;
     cin >> T;
+
+
+
+    // return 0;
     for (int i = 1; i <= T; i++)
     {
         solve();
