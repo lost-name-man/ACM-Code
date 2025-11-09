c593d033491f6ea6136cf63644fb64d70f808b56
lost-name-man <3014326408@qq.com>
2025-11-09 16:20:35 +0800

    1

--- jctmp.cpp
+++ jctmp.cpp
@@ -4,44 +4,53 @@ using namespace std;
 #define endl '\n'
 const int INF = 1e18 + 3;
 
-int n;
-vector<int> arr;
-string s;
-void solve()
+int a, b;
+int maxop;
+int checkans(int num, int a, int b)
 {
     int cnt = 0;
-
-    cin >> n >> s;
-    arr = vector<int>(n + 5);
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= maxop; i++)
     {
-        cin >> arr[i];
-        if (arr[i] != i)
+        if (num & 1)
+        {
+            a -= __gcd(a, b);
+        }
+        else
+        {
+            b -= __gcd(a, b);
+        }
+        num >>= 1;
+        cnt++;
+        if (a == 0 || b == 0)
         {
             cnt++;
+            break;
         }
     }
+    return cnt;
+}
 
-    if (n == 2 || n == 3 && arr[1] == 2 && arr[2] == 3 && arr[3] == 1 && s == "Bob")
-    {
-        cout << "Alice" << endl;
-        return;
-    }
+void dfs(int a, int b, int deep)
+{
+}
 
-    if (cnt == 2 && s == "Alice" || cnt == 0 && s == "Bob")
+void solve()
+{
+    cin >> a >> b;
+    int ans = 30;
+
+    maxop = log2(a) * 2 + 2;
+    for (int i = 0; i < (1ll << maxop); i++)
     {
-        cout << "Alice" << endl;
+        ans = min(ans, checkans(i, a, b));
     }
-    else
-    {
-        cout << "Bob" << endl;
-    }
+    cout << ans << endl;
 }
 
 signed main()
 {
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int T = 1;
     cin >> T;
     for (int i = 1; i <= T; i++)
