daa459ec43d5f8f970e0dc3e54e9c68e177ec74d
lost-name-man <3014326408@qq.com>
2025-11-10 22:35:46 +0800

    nanjing

--- jctmp.cpp
+++ jctmp.cpp
@@ -4,72 +4,106 @@ using namespace std;
 #define endl '\n'
 const int INF = 1e18 + 3;
 
-int n, k;
-int sumop;
-int work1(int cn)
+int n, k, m;
+map<int, map<int, int>> pval;
+vector<int> isf;
+vector<pair<int, int>> val;
+
+struct Node
 {
-    int cnt = 0;
-
-    while (cn > 1)
+    int a;
+    int b;
+    int val;
+    const bool operator<(const Node &others) const
     {
-        cn -= ceil(cn * 1.0 / k);
-        cnt++;
+        return this->val > others.val;
     }
-
-    int index = 1;
-    for (int i = 1; i <= cnt; i++)
-    {
-        index = index + ceil(index * 1.0 / (k - 1));
-    }
-    return index;
-}
-int work2(int cn)
-{
-    int cnt = 0;
-    while (cn > 1)
-    {
-        int nowde = ceil(cn * 1.0 / k);
-
-        if (nowde == 1)
-        {
-            cnt += cn - 1;
-            cn = 1;
-            continue;
-        }
-        int be_next_num = cn - ((nowde - 1) * k + 1) + 1; // 成为下一个至少需要减多少
-        int to_next_turn = ceil(be_next_num * 1.0 / nowde);
-
-        cnt += to_next_turn;
-        cn -= to_next_turn * nowde;
-    }
-
-    int nowcnt = 0;
-    int index = 1;
-    while (nowcnt < cnt)
-    {
-        int addnum = ceil(index * 1.0 / (k - 1));
-        int be_next_num = (addnum) * (k - 1) + 1; // 前面有addnum个-1与一个1
-        int to_next_turn = ceil((be_next_num - index) * 1.0 / addnum);
-
-        int turn = min(cnt - nowcnt, to_next_turn);
-        index = index + turn * addnum;
-        nowcnt += turn;
-    }
-
-    return index;
-}
+};
 void solve()
 {
-    cin >> n >> k;
-    int ans;
-    if (k < 1e9 && k * k <= n)
+    cin >> n >> m >> k;
+    isf = vector<int>(k + 5);
+    val = vector<pair<int, int>>(k + 5);
+    pval = map<int, map<int, int>>();
+
+    int ans = 0;
+    for (int i = 1; i <= k; i++)
     {
-        ans = work1(n);
+        val[i].second = i;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        isf[tmp] = 1;
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == b)
+        {
+            if (isf[a] == 1)
+            {
+                ans++;
+            }
+            else
+            {
+                val[a].first++;
+            }
+            continue;
+        }
+        if (a > b)
+        {
+            swap(a, b);
+        }
+        if (isf[a] == 1 && isf[b] == 1)
+        {
+            ans++;
+        }
+        else if (isf[a] == 1)
+        {
+            val[b].first++;
+        }
+        else if (isf[b] == 1)
+        {
+            val[a].first++;
+        }
+        else
+        {
+            pval[a][b]++;
+        }
+    }
+
+    vector<Node> st;
+    for (auto it1 = pval.begin(); it1 != pval.end(); it1++)
+    {
+        for (auto it2 = it1->second.begin(); it2 != it1->second.end(); it2++)
+        {
+            it2->second += val[it1->first].first + val[it2->first].first;
+            st.push_back({it1->first, it2->first, it2->second});
+        }
+    }
+
+    sort(st.begin(), st.end());
+    sort(val.begin() + 1, val.begin() + 1 + k, greater<pair<int, int>>());
+    int add = 0;
+    if (!st.empty())
+    {
+        add = st.begin()->val;
+    }
+
+    if (k - n == 1)
+    {
+        add = max(add, val[1].first);
     }
     else
     {
-        ans = work2(n);
+        int a = min(val[1].second, val[2].second), b = max(val[1].second, val[2].second);
+        add = max(add, val[1].first + val[2].first);
     }
+    ans += add;
     cout << ans << endl;
 }
 
