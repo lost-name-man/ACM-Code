9cc0cc1572767108268d0c0e45c27613203c8564
lost-name-man <3014326408@qq.com>
2025-11-11 23:24:23 +0800

    tmp

--- jctmp.cpp
+++ jctmp.cpp
@@ -4,107 +4,86 @@ using namespace std;
 #define endl '\n'
 const int INF = 1e18 + 3;
 
-int n, k, m;
-map<int, map<int, int>> pval;
-vector<int> isf;
-vector<pair<int, int>> val;
+int tot = 1;
 
-struct Node
+int n, m;
+vector<int> p;
+vector<map<int, int>> g;
+int pindex;
+
+vector<int> du, vis;
+vector<pair<int, int>> eans;
+int ans = 0;
+
+void dfs(int nownode, int fa)
 {
-    int a;
-    int b;
-    int val;
-    const bool operator<(const Node &others) const
-    {
-        return this->val > others.val;
-    }
-};
-void solve()
-{
-    cin >> n >> m >> k;
-    isf = vector<int>(k + 5);
-    val = vector<pair<int, int>>(k + 5);
-    pval = map<int, map<int, int>>();
 
-    int ans = 0;
-    for (int i = 1; i <= k; i++)
+    vis[nownode] = 1;
+    int visnum = 1;
+    if (fa == 0)
     {
-        val[i].second = i;
+        visnum = 0;
     }
-    for (int i = 1; i <= n; i++)
+    while (visnum < du[nownode] && pindex <= n)
     {
-        int tmp;
-        cin >> tmp;
-        isf[tmp] = 1;
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-
-        if (a == b)
+        int neednode = p[pindex];
+        if (vis[neednode] == 1)
         {
-            if (isf[a] == 1)
-            {
-                ans++;
-            }
-            else
-            {
-                val[a].first++;
-            }
+            visnum++;
             continue;
         }
-        if (a > b)
+        if (g[nownode][neednode])
         {
-            swap(a, b);
-        }
-        if (isf[a] == 1 && isf[b] == 1)
-        {
-            ans++;
-        }
-        else if (isf[a] == 1)
-        {
-            val[b].first++;
-        }
-        else if (isf[b] == 1)
-        {
-            val[a].first++;
+            pindex++;
+            dfs(neednode, nownode);
         }
         else
         {
-            pval[a][b]++;
+            du[nownode]++;
+            du[neednode]++;
+            g[neednode][nownode]++;
+            g[nownode][neednode]++;
+            pindex++;
+            ans++;
+            eans.push_back({nownode, neednode});
+            dfs(neednode, nownode);
         }
+        visnum++;
+    }
+}
+
+void solve()
+{
+    ans = 0;
+    cin >> n >> m;
+    p = du = vis = vector<int>(n + 5);
+    g = vector<map<int, int>>(n + 5, map<int, int>());
+    pindex = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        du[x]++;
+        du[y]++;
+        g[x][y] = 1;
+        g[y][x] = 1;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> p[i];
     }
 
-    vector<Node> st;
-    for (auto it1 = pval.begin(); it1 != pval.end(); it1++)
+    while (pindex <= n)
     {
-        for (auto it2 = it1->second.begin(); it2 != it1->second.end(); it2++)
-        {
-            it2->second += val[it1->first].first + val[it2->first].first;
-            st.push_back({it1->first, it2->first, it2->second});
-        }
+        int neednode = p[pindex];
+        pindex++;
+        dfs(neednode, 0);
     }
-
-    sort(st.begin(), st.end());
-    sort(val.begin() + 1, val.begin() + 1 + k, greater<pair<int, int>>());
-    int add = 0;
-    if (!st.empty())
-    {
-        add = st.begin()->val;
-    }
-
-    if (k - n == 1)
-    {
-        add = max(add, val[1].first);
-    }
-    else
-    {
-        int a = min(val[1].second, val[2].second), b = max(val[1].second, val[2].second);
-        add = max(add, val[1].first + val[2].first);
-    }
-    ans += add;
     cout << ans << endl;
+    for (int i = 0; i < eans.size(); i++)
+    {
+        cout << eans[i].first << ' ' << eans[i].second << endl;
+    }
 }
 
 signed main()
@@ -112,7 +91,7 @@ signed main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     for (int i = 1; i <= T; i++)
     {
         solve();
