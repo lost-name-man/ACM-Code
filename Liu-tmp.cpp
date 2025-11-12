92bf6045591bfdb378d1a5c6a8c47eb9278d3245
BorderSu <suqin1329@gmail.com>
2025-11-12 20:10:54 +0800

    ttmp

--- Liu-tmp.cpp
+++ Liu-tmp.cpp
@@ -20,123 +20,143 @@ using namespace std;
 #define int long long
 #define endl '\n'
 const int INF = 1e18 + 3;
-const int MOD = 998244353;
 
+int n;
+vector<int> arr, brr, crr;
 
-map<string, int>val_exist;
-
-int do3(int a, int b, map<int, int>&arr)
+bool p(int num)
 {
-    arr[a|b]=1;
-    arr[a&b]=1;
-    arr[a^b]=1;
-
-    return 0;
-}
-
-string do33(int a, int b)
-{
-
-    string einzwei;
-
-    string astr, bstr;
-
-
-    while(a)
+    //vector<int> pre(n + 5);
+    // int cnt = 0;
+    vector<int> inv1, inv2;
+    for (int i = 1; i <= n; i++)
     {
-        int i=a%2;
-        astr+=(char)(i+'0');
-        a/=2;
-    }
-    while(astr.size()<32)
-    {
-        astr+='0';
-    }
-
-    while(b)
-    {
-        int i=b%2;
-        bstr+=(char)(i+'0');
-        b/=2;
-    }
-    while(bstr.size()<32)
-    {
-        bstr+='0';
-    }
-
-    // cout<<astr<<endl;
-
-
-    for(int i=0; i<32; i++)
-    {
-        if(astr[i]=='1' || bstr[i]=='1')
+        if (arr[i] == 0)
         {
-            einzwei[i]='1';
-        }
-    }
-    
-    
-
-    return einzwei;
-
-}
-
-void solve()
-{
-    int n;
-    cin>>n;
-
-    int ori=0;
-
-    vector<int>arr(n+5, 0);
-    for(int i=1; i<=n; i++)
-    {
-        cin>>arr[i];
-
-        if(i>1)
-        {
-            val_exist[do33(arr[i-1], arr[i])]=1;
-        }
-        
-    }
-
-    int ans=0;
-
-    int nullnum=0;
-    for(auto iter=val_exist.begin(); iter!=val_exist.end(); iter++)
-    {
-        int einnum=0;
-        for(int i=0; i<32; i++)
-        {
-            if(iter->first[i]=='1')
+            if (brr[i] >= num)
             {
-                einnum++;
+                inv1.push_back(1);
+            }
+            continue;
+        }
+        int lx = (num - brr[i])/ arr[i];
+        int exp = (((num - brr[i]) % arr[i] ) != 0 );
+        
+        int tmpa = (num - brr[i]) , tmpb=arr[i];
+        if(tmpa>0 && tmpb<0 ||tmpa>0 && tmpb<0)
+        {
+
+        }
+        else {
+        
+            if (arr[i] > 0)
+            {
+                int lindex;
+                if(exp)
+                {
+                    lindex = (upper_bound(crr.begin() + 1, crr.begin() + 1 + n, lx) - crr.begin());
+                }
+                else 
+                {
+                    lindex = (lower_bound(crr.begin() + 1, crr.begin() + 1 + n, lx) - crr.begin());
+                }
+                
+                inv1.push_back(lindex);
+            
+            }
+            else
+            {
+                int rindex;
+                
+                if(exp)
+                {
+                    rindex = (upper_bound(crr.begin() + 1, crr.begin() + 1 + n, lx) - crr.begin());
+                }
+                else 
+                {
+                    rindex = (upper_bound(crr.begin() + 1, crr.begin() + 1 + n, lx) - crr.begin());
+                }
+                inv2.push_back(rindex - 1);
             }
         }
-        if(einnum!=0)
+    }
+    sort(inv1.begin(), inv1.end(),greater<int>());
+    sort(inv2.begin(), inv2.end());
+    
+    int nownum = 0;
+    int index1 = n, index2 = 1;
+    for (int i = 0; i < inv1.size(); i++)
+    {
+        if (index1 >= inv1[i] && index1>=index2)
         {
-            ans+=pow(2, einnum);
+            index1--;
+            nownum++;
+        }
+    }
+    for (int i = 0; i < inv2.size(); i++)
+    {
+        if (index2 <= inv2[i] && index1 >= index2)
+        {
+            index2++;
+            nownum++;
+        }
+    }
+    if (nownum >= (n / 2 + n % 2))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void solve()
+{
+    cin >> n;
+    arr = brr = crr = vector<int>(n + 5);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> brr[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> crr[i];
+    }
+    sort(crr.begin() + 1, crr.begin() + 1 + n);
+
+    int l = -2 * (1e18), r = 2 * 1e18;
+    int ans = 0;
+    while (l <= r)
+    {
+        int mid=l/2+r/2;
+        if(l%2==1 && r%2==1)
+        {
+            mid++;
+        }
+        // int mid = (l + r) / 2;
+        if (p(mid))
+        {
+            l = mid + 1;
+            ans = mid;
         }
         else
         {
-            nullnum=1;
+            r = mid - 1;
         }
-        
     }
-
-
-    cout<<ans+nullnum<<endl;
-
+    cout << ans << endl;
 }
 
 signed main()
 {
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int T = 1;
-    //  cin >> T;
-
-    return 0;
+    cin >> T;
     for (int i = 1; i <= T; i++)
     {
         solve();
--- tmpzwei.cpp
+++ tmpzwei.cpp
@@ -16,222 +16,77 @@
 #include <unordered_map>
 #include <numeric>
 #include <iomanip>
+
 using namespace std;
 #define int long long
-#define endl '\n'
+// #define endl '\n'
 const int INF = 1e18 + 3;
-const int MOD = 1e9+7;
-
-
-
-int f[305][305][305][3]={0};
-int ans[305][305][305]={0};
-
+const int MOD = 998244353;
 void solve()
 {
-    int n, q;
-    cin>>n>>q;
-    string str;
-    cin>>str;
-    str="@"+str;
-    
+    int n, k;
+    cin>>n>>k;
 
-    vector<int>prequo(n+5, 0);
 
+    priority_queue<int>pq;
+
+    int last=0;
     for(int i=1; i<=n; i++)
     {
-        prequo[i]+=prequo[i-1];
-        if(str[i]=='?')
-        {
-            prequo[i]++;
-        }
+
+        int x;
+        cin>>x;
+        pq.push(x);
+        last=max(last, x);
     }
 
-    //a==0 b==1 c==2
-
-    if(str[1]=='?')
-    {
-        
-        f[1][1][0][0]=1;
-        f[1][0][1][1]=1;
-        f[1][0][0][2]=1;
-    }
-    else
-    {
-        f[1][0][0][str[1]-'a']=1;
-    }
-
+    last%=MOD;
     
-    for(int i=2; i<=n; i++)
-    {
-        for(int j=0; j<=prequo[i]; j++)
-        {
-            for(int k=0; k<=prequo[i]-j; k++)
-            {
-                int l=prequo[i]-j-k;
-
-
-                if(str[i]!='?')
-                { 
-                    int nowch=str[i]-'a';
-
-                    for(int rch=0; rch<=2; rch++)
-                    {
-                        if(rch==nowch)
-                        {
-                            continue;
-                        }
-                        f[i][j][k][nowch] += f[i-1][j][k][rch];
-                    }
-                }
-                else
-                {
-
-                    // ------------------------------------
-                    // A
-                    if(j)
-                    {
-                        f[i][j][k][0] += f[i-1][j-1][k][1];
-                        f[i][j][k][0] += f[i-1][j-1][k][2];
-                        f[i][j][k][0] %=MOD;
-                    }
-
-                    // B
-                    if(k)
-                    {
-                        f[i][j][k][1] += f[i-1][j][k-1][0];
-                        f[i][j][k][1] += f[i-1][j][k-1][2];
-                        f[i][j][k][1] %=MOD;
-                    }
-
-                    // C
-                    if(l)
-                    {
-                        f[i][j][k][2] += f[i-1][j][k][0];
-                        f[i][j][k][2] += f[i-1][j][k][1];
-                        f[i][j][k][2] %=MOD;
-
-                    }
-
-
-                    // ------------------------------------
-                }
-                
-            }
-        }
-    }
-
-
+    pq.pop();
     
 
-    int quonum=prequo[n];
-
-
-    for(int j=0; j<=quonum; j++)
+    while(pq.size()>=(k-1))
     {
-        for(int k=0; k<=quonum; k++)
+        int nowval=last;
+        int nullnum=0;
+        for(int i=2; i<=k; i++)
         {
-            int l=quonum-j-k;
 
-            for(int abc=0; abc<=2; abc++)
+            if(pq.top()==0)
             {
-                ans[j][k][l]+=f[n][j][k][abc];
+                nullnum++;
+                break;
             }
-                
+
+            nowval*=(pq.top())%MOD;
+            nowval%=MOD;
+            pq.pop();
             
         }
-    }
 
-
-    for(int j=0; j<=quonum; j++)
-    {
-        for(int k=0; k<=quonum; k++)
+        if(nullnum)
         {
-            for(int l=0; l<=quonum; l++)
-            {
-                cout<<"! "<<j<<" "<<k<<" "<<l<<" ==";
-                cout<<ans[j][k][l]<<endl;
-            }
-
+            break;
+        }
+        else
+        {
+            last=nowval;
+            last%=MOD;
         }
     }
 
 
-
-
-    // -----------------
-    for(int j=1; j<=quonum; j++)
-    {
-        for(int k=0; k<=quonum; k++)
-        {
-            for(int l=0; l<=quonum; l++)
-            {
-                for(int abc=0; abc<=2; abc++)
-                {
-                    ans[j][k][l]+=ans[j-1][k][l];
-                }
-                
-            }
-        }
-    }
-    for(int j=0; j<=quonum; j++)
-    {
-        for(int k=1; k<=quonum; k++)
-        {
-            for(int l=0; l<=quonum; l++)
-            {
-                for(int abc=0; abc<=2; abc++)
-                {
-                    ans[j][k][l]+=ans[j][k-1][l];
-                }
-                
-            }
-        }
-    }
-    for(int j=0; j<=quonum; j++)
-    {
-        for(int k=0; k<=quonum; k++)
-        {
-            for(int l=1; l<=quonum; l++)
-            {
-                for(int abc=0; abc<=2; abc++)
-                {
-                    ans[j][k][l]+=ans[j][k][l-1];
-                }
-            }
-        }
-    }
-
-
-    
-
-    for(int i=1; i<=q; i++)
-    {
-        int x, y, z;
-
-        cin>>x>>y>>z;
-        x=min(x, quonum);
-        y=min(y, quonum);
-        z=min(z, quonum);
-
-
-
-        cout<<ans[x][y][z]<<endl;
-
-    }
-
-
-
+    cout<<last%MOD<<endl;
 
 }
 
 signed main()
 {
+
     // ios::sync_with_stdio(0);
     // cin.tie(0);
     int T = 1;
-    //  cin >> T;
-
+    cin >> T;
     for (int i = 1; i <= T; i++)
     {
         solve();
